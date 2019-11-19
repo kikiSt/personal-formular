6f1dd009eebaef122a70bc3cd95e67db27f4b8d8
Bastian Steingruber <steingruberbastian@gmail.com>
2019-11-19 10:49:20 +0100

    Initialized gradle

---
+++ src/test/java/tbz1/personalformular/PersonalformularApplicationTests.java
@@ -0,0 +1,13 @@
+package tbz1.personalformular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalformularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
